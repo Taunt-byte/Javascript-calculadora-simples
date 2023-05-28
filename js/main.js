3fbd7bd4d9f4e3e0f7057d1ec28fbac61fd15c9b
Lucas Neves <lucasluan94@hotmail.com>
2023-05-28 16:59:20 -0300

    UPDATE CSS files

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
 "use strict";
 
 var input = document.getElementById("input"), // botao de input e output
-    numero = document.querySelector(".numero div"),//botao de numero
+    numero = document.querySelector(".numero div")//botao de numero
+
+// Função para adicionar dois números
+function adicionar(num1, num2) {
+    return num1 + num2;
+}
+
+// Função para subtrair dois números
+function subtrair(num1, num2) {
+    return num1 - num2;
+}
+
+// Função para multiplicar dois números
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+
+// Função para dividir dois números
+function dividir(num1, num2) {
+    if (num2 === 0) {
+        return "Erro: Não é possível dividir por zero!";
+    }
+    return num1 / num2;
+}
